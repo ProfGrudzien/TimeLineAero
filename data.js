9ef1c4bdf611dd551e62f95da9e40958436ee8cb
Grudzien <70582964+ProfGrudzien@users.noreply.github.com>
2022-05-13 16:03:16 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,117 @@
+const cartes = [
+    {
+        recto: "Première boucle : début de l'acrobatie aérienne ?",
+        verso: "Adolphe Pégoud à bord du Blériot XI effectue le 21 septembre 1913 la première boucle aérienne.",
+        date: "1913-10-21"
+    },
+    {
+        recto: "Création de la Société des lignes Latécoère ?",
+        verso: "Le 25 décembre 1918, Pierre-Georges Latécoère fonde la Société des lignes Latécoère qui deviendra par la suite la Compagnie générale aéropostale.",
+        date: "1918-12-25"
+    },
+    {
+        recto: "Décennie de conception du Fouga magistère ?",
+        verso: "Le fouga magistère est un avion de conception des années 50.",
+        date: "1950"
+    },
+    {
+        recto: "Replacement du Fouga Magister par l'Alpha Jet ?",
+        verso: "En 1981, l'Alpha Jet remplace le Fouga Magistère au sein de la patrouille de France.",
+        date: "1981"
+    },
+    {
+        recto: "Création de la première patrouille de France ?",
+        verso: "La première patrouille de France (patrouille d'Étampes) a été créée en 1931.",
+        date: "1931"
+    },
+    {
+        recto: "Création de l'École de l'air ?",
+        verso: "L'École de l'air a été créée le 2 juillet 1934 sur proposition du Ministre de l’air Pierre Cot.",
+        date: "1934-07-02"
+    },
+    {
+        recto: "Création de la P.A.F. ?",
+        verso: "La Patrouille Accrobatique de France a été créée le 14 septembre 1953.",
+        date: "1953-09-14"
+    },
+    {
+        recto: "Premier pilote à franchir le mur du son ?",
+        verso: "Le 14 octobre 1947, Chuck Yeager fut le premier pilote à franchir le mur du son.",
+        date: "1947-10-14"
+    },
+    {
+        recto: "Premier passage du mur du son en chute libre ?",
+        verso: "Le 14 octobre 2012, Félix Baumgartner fut le premier à franchir le mur du son en chute libre.",
+        date: "2012-10-14"
+    },
+    {
+        recto: "Premier vol du Concorde",
+        verso: "Le premier vol du Concorde a eu lieu le 2 mars 1969.",
+        date: "1969-03-02"
+    },
+    {
+        recto: "Premier vol du Boeing 747",
+        verso: "Le premier vol du Boeing 747 a eu lieu le 9 février 1969.",
+        date: "1969-02-09"
+    },
+    {
+        recto: "Contournement de la Tour Eiffel en dirigeable ?",
+        verso: "Alberto Santos Dumont a réalisé un contournement de la Tour Eiffel en dirigeable le 8 août 1901.",
+        date: "1901-08-08"
+    },
+    {
+        recto: "Première traversée de la Manche ?",
+        verso: "Le 25 juillet 1909, Louis Blériot réalise la première traversée de la Manche.",
+        date: "1909-07-25"
+    },
+    {
+        recto: "Première traversée de l'Atlantique en avion sans étapes ?",
+        verso: "Le 21 mai 1927, Charles Lindbergh réalise la première traversée de l'Atlantique en avion sans étapes à bord du Spirit of Saint Louis.",
+        date: "1927-05-21"
+    },
+    {
+        recto: "Atterrissage en urgence d'un A320 sur l'Hudson ?",
+        verso: "Le 15 janvier 2009, Chesley Sullenberger réalise un atterrissage en urgence à bord d'un A320 suite à une collision aviaire.",
+        date: "2009-01-15"
+    },
+    {
+        recto: "Premiers pas sur la Lune ?",
+        verso: "Le 21 juillet 1969, lors de la mission Apollo 11, Neil Armstrong est le premier à poser le pied sur la Lune.",
+        date: "1969-07-21"
+    },
+    {
+        recto: "Premier Homme dans l'espace ?",
+        verso: "Le 12 avril 1961, Youri Gagarine est le premier Homme dans l'espace, lors de la mission Vostok-1.",
+        date: "1961-04-12"
+    },
+    {
+        recto: "Premier lancement de la navette Columbia ?",
+        verso: "Le premier lancement de la navette Columbia a eu lieu le 12 avril 1981.",
+        date: "1981-04-12"
+    },
+    {
+        recto: "Premier lancement habité de Space X ?",
+        verso: "Le premier lancement habité de Space X a eu lieu Le 30 mai 2020.",
+        date: "2020-05-30"
+    },
+    {
+        recto: "Franchissement de la cordillère des Andes ?",
+        verso: "Adrienne Bolland est la première à franchir la cordillère des Andes le premier avril 1921.",
+        date: "1921-04-01"
+    },
+    {
+        recto: "Première femme seule aux commandes de son avion à traverser l'océan Atlantique ?",
+        verso: "Amelia Earhart à traversé l'océan Atlantique en seule au commande d'un Lockheed Vega le 20 mai 1932.",
+        date: "1932-05-20"
+    },
+    {
+        recto: "Fondation de Air France ?",
+        verso: "Air France a été fondée le 7 octobre 1933.",
+        date: "1933-10-07"
+    },
+    {
+        recto: "Première élévation d'un hélicoptère ?",
+        verso: "Paul Cornu fut le premier à réaliser un vol en hélicoptère le 13 novembre 1907",
+        date: "1907-11-13"
+    }
+    ];
---
+++ script.js
@@ -0,0 +1,160 @@
+const divTimeLine = document.querySelector(".timeline")
+const btnNbJoueurs = document.getElementById("btnNbJoueurs")
+const btnCommencer = document.getElementById("btnCommencer")
+const listeJoueurs = []
+const TimeLine = []
+var carteAJouer = null
+var joueurEnCours = -1
+
+
+function handleClick(event) {
+    if (event.target == btnNbJoueurs) {
+        validerNbJoueurs(event)
+    } else if (event.target == btnCommencer) {
+        commencer(event)
+    } else if (event.target.className == "insert") {
+        const i = Array.from(event.target.parentNode.children).indexOf(event.target) / 2
+        if (verifierTimeLine(carteAJouer, i)) {
+            document.getElementById("message").textContent = `Bravo ${listeJoueurs[joueurEnCours].pseudo} !`
+            listeJoueurs[joueurEnCours].score += 1
+            listeJoueurs[joueurEnCours].tdPoints.textContent = listeJoueurs[joueurEnCours].score
+            insereTimeLine(carteAJouer)
+        } else {
+            document.getElementById("message").textContent = `Dommage  ${listeJoueurs[joueurEnCours].pseudo} ...`
+        }
+        victoire()
+    }
+}
+
+function validerNbJoueurs(event) {
+    const errorNbJoueurs = document.getElementById("errorNbJoueurs")
+    const divNbJoueurs = document.getElementById("divNbJoueurs")
+    const nbJoueurs = document.getElementById("entryNbJoueurs").value
+    if (nbJoueurs < 1) {
+        errorNbJoueurs.textContent = "Le nombre de joueur est incorrect."
+    } else {
+        divNbJoueurs.innerHTML = ""
+        for (let i=0; i<nbJoueurs; i++) {
+            listeJoueurs.push({nom: "Joueur1", score:0})
+            const p = document.createElement("p")
+            p.innerHTML = `<label for="pseudoJ${i+1}">Nom du joueur n°${i+1} :</label>`
+            p.innerHTML += `<input type="text" id="pseudoJ${i+1}" placeholder="Joueur n°${i+1}"/>`
+            divNbJoueurs.appendChild(p)
+        }
+        btnCommencer.style.display = "inline-block"
+    }
+}
+
+function commencer(event) {
+    /* Création des joueurs */
+    for (let i=0; i<listeJoueurs.length; i++) {
+        const table = document.querySelector(".scores table")
+        const joueur = listeJoueurs[i]
+        joueur.pseudo = document.getElementById(`pseudoJ${i+1}`).value || `Joueur n°${i+1}`
+        const tr = document.createElement("tr")
+        const tdPseudo = document.createElement("td")
+        const tdPoints = document.createElement("td")
+        tdPseudo.textContent = joueur.pseudo
+        tdPoints.textContent = 0
+        tr.appendChild(tdPseudo)
+        tr.appendChild(tdPoints)
+        table.appendChild(tr)
+        joueur.tdPoints = tdPoints
+    }
+    /* Placement des premières cartes */
+    const divTitre = document.querySelector(".titre");
+    joueurSuivant()
+    divTimeLine.appendChild(construireInsert())
+    for (let i=0; i<3; i++) {
+        insereTimeLine(piocherCarte())
+    }
+    /* Affichage */
+    document.querySelector(".masque").style.display = "None"
+}
+
+function joueurSuivant() {
+    joueurEnCours = (joueurEnCours+1)%listeJoueurs.length
+    const divTitre = document.querySelector(".titre");
+    while (divTitre.firstChild) {
+      divTitre.removeChild(divTitre.firstChild);
+    }
+    const h = document.createElement("h2")
+    h.textContent = `C'est au tour de ${listeJoueurs[joueurEnCours].pseudo}.`
+    divTitre.appendChild(h)
+    carteAJouer = piocherCarte()
+    divTitre.appendChild(construireCarte(carteAJouer, false))
+}
+
+function construireInsert() {
+    const div = document.createElement("div")
+    div.className = "insert"
+    return div
+}
+
+function construireCarte(carte, jouee) {
+    const div = document.createElement("div")
+    div.className = "carte"
+    const span = document.createElement("span")
+    span.textContent = jouee ? carte.verso : carte.recto
+    div.appendChild(span)
+    return div
+}
+
+function piocherCarte() {
+    const i = Math.floor(Math.random()*cartes.length)
+    return cartes.splice(i, 1)[0]
+}
+
+function insereTimeLine(carte) {
+    /* insérer une carte au bon endroit dans la TimeLine */
+    carte.div = construireCarte(carte, true)
+    var i=0
+    while (i<TimeLine.length && carte.date>TimeLine[i].date) {i++}
+    if (i==TimeLine.length) {
+        divTimeLine.appendChild(carte.div)
+        divTimeLine.appendChild(construireInsert())
+    } else {
+        const insert = construireInsert()
+        divTimeLine.insertBefore(insert, TimeLine[i].div);
+        divTimeLine.insertBefore(carte.div, insert);
+    }
+    TimeLine.splice(i, 0, carte)
+}
+
+function verifierTimeLine(carte, i) {
+    /* vérifie si la position i est la bonne position de la carte dans la TimeLine */
+    if (i == 0) {
+        return (carte.date <= TimeLine[0].date)
+    } else if (i == TimeLine.length) {
+        return (carte.date >= TimeLine[TimeLine.length-1].date)
+    } else {
+        return (carte.date >= TimeLine[i-1].date && carte.date <= TimeLine[i].date)
+    }
+}
+
+function victoire() {
+    if (cartes.length == 0) {
+        var imax = 0
+        var max = listeJoueurs[0].score
+        for (let i=0; i<listeJoueurs.length; i++) {
+            if (listeJoueurs[i].score > max) {
+                max = listeJoueurs[i].score
+                imax = i
+            }
+        }
+        const gagnant = listeJoueurs[imax].pseudo
+        document.querySelector(".titre").innerHTML = `<h1>Bravo ${gagnant} ! Vous avez gagné !</h1>`
+        document.querySelectorAll(".insert").forEach(element => {element.style.display = "None"})
+        document.getElementById("message").style.display = "None"
+    } else if (listeJoueurs[joueurEnCours].score == 10) {
+        const gagnant = listeJoueurs[joueurEnCours].pseudo
+        document.querySelector(".titre").innerHTML = `<h1>Bravo ${gagnant} !</h1>`
+        document.querySelectorAll(".insert").forEach(element => {element.style.display = "None"})
+        document.getElementById("message").style.display = "None"
+    } else {
+        joueurSuivant()
+    }
+}
+
+btnCommencer.style.display = "None"
+document.addEventListener("click", handleClick)
